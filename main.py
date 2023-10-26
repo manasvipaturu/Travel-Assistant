4110f64f1f93694fa2aad66ef840fa3d439fed91
manasvipaturu <90577048+manasvipaturu@users.noreply.github.com>
2023-10-25 20:09:18 -0500

    Create main.py
    entry point for project

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+import argparse
+from assistant import TravelAssistant
+
+def main():
+    parser = argparse.ArgumentParser(description="Travel Assistant")
+    parser.add_argument("--query", type=str, help="Query for the travel assistant")
+    args = parser.parse_args()
+
+    assistant = TravelAssistant()
+    response = assistant.answer_question(args.query)
+    print(response)
+
+if __name__ == "__main__":
+    main()
