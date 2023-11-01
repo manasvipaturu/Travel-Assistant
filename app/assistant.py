96ebcc266e11b41e252804ca0a20600585f923b7
Manasvi Paturu <90577048+manasvipaturu@users.noreply.github.com>
2023-10-31 20:29:07 -0500

    Update assistant.py

--- app/assistant.py
+++ app/assistant.py
@@ -1,11 +1,14 @@
-#assistant
+# Import necessary libraries
 import openai
 import json
 
+# Define a Travel Assistant class
 class TravelAssistant:
     def __init__(self):
-        openai.api_key = 'API_KEY'
+        # Set the OpenAI API key
+        openai.api_key = 'API_KEY'  # Replace 'API_KEY' with your actual API key
 
+    # Answer a travel-related question
     def answer_question(self, question):
         response = openai.Completion.create(
             engine="text-davinci-002",
@@ -14,18 +17,26 @@ class TravelAssistant:
         )
         return response.choices[0].text
 
+    # Provide recommendations for a specific city
     def recommend_city(self, city):
+        # Load city data from a JSON file
         with open('data/city_data.json', 'r') as file:
             city_data = json.load(file)
+
+            # Check if the city is in the data
             if city in city_data:
                 hotels = city_data[city]['hotels']
                 restaurants = city_data[city]['restaurants']
                 activities = city_data[city]['activities']
 
+                # Load a response template
                 with open('templates/responses/recommendations.txt', 'r') as template_file:
                     template = template_file.read()
-                    response = template.format(city=city, hotels=', '.join(hotels), restaurants=', '.join(restaurants), activities=', '.join(activities))
+
+                # Populate the response template with data
+                response = template.format(city=city, hotels=', '.join(hotels), restaurants=', '.join(restaurants), activities=', '.join(activities))
 
         return response
+
 
 
